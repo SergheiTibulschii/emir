ae18fe082e9d2317eb52a0e5485b03d0c253dd35
serghei.Tibulschii@gmail.com <serghei.Tibulschii@gmail.com>
2020-07-26 10:17:51 +0300

    fix

--- src/js/components/Social.js
+++ src/js/components/Social.js
@@ -3,7 +3,11 @@ import { h } from 'preact';
 export const Social = ({ link, url, alt, className }) => {
     return (
         <div className="social">
-            <a href={link} className={`inline-block ${className}`}>
+            <a
+                href={link}
+                target="_blank"
+                className={`inline-block ${className}`}
+            >
                 <img src={url} alt={alt} className="w-full" />
             </a>
         </div>
